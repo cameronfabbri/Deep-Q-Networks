e19bd478a70dd9ea0267735c50dfe1c7af1e624e
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-13 17:26:36 -0500

    working on training

--- breakout.py
+++ breakout.py
@@ -3,6 +3,22 @@ from collections import deque
 import cv2
 from tqdm import tqdm
 from random import randint
+import tensorflow as tf
+import architecture as arch
+
+def get_feed_dict(batch_size, images_placeholder, seq_length, replay_database):
+
+   # get the size of the database
+   replay_size = len(replay_database)
+
+   start = randint(0, replay_size-seq_length)
+
+   feed_dict = {
+      images_placeholder: original_images,
+   }
+
+   return feed_dict
+
 
 '''
    Training will sample randomly
@@ -16,9 +32,6 @@ from random import randint
 '''
 def train(replay_database, seq_length, SHAPE):
 
-   # Don't need to import until we're actually using them
-   import tensorflow as tf
-   import architecture as arch
 
    # set up computational graph
    with tf.Graph().as_default():
@@ -26,23 +39,42 @@ def train(replay_database, seq_length, SHAPE):
       
       # tensor of size batch, width, height, channels*seq_length
       images_placeholder = tf.placeholder(tf.float32, shape=(batch_size, SHAPE[1], SHAPE[0], 3*seq_length)) 
+      next_step_image    = tf.placeholder(tf.float32, shape=(1, SHAPE[1], SHAPE[0], 3))
 
-      # grab random batch
+      # Q values for each action
+      target_value = architecture.inference(images_placeholder, 'train')
+      actual_value = architecture.inference(next_step_image, 'train')
 
-
-
+      loss = architecture.loss(target_value, actual_value)
+      
+      '''
+         grab random batch -> replay database must be of size larger than batch_size*seq_length
+         pick random start spot. Ensures it does not extend the db and also only picks things
+         once per iteration
+      '''
 
       while True:
          # start a new game
          initial_observation = env.reset()
 
+         '''
+           The input is the current state image
+           The output is the predicted Q value for each action
+           Loss: Net(s_i)(a_i) - (r_i+1 max(Net(s_i+1)))
+         '''
+
+         # get sequence and run through network to get max action
+         feed_dict = get_feed_dict(batch_size, images_placeholder, seq_length, replay_database)
+
+         
+
          action = env.action_space.sample()
          state, reward, done, info, = env.step(action)
 
-         if done:
-            game_num += 1
-            print 'Game number: ', game_num
-            observation = env.reset()
+         #if done:
+         #   game_num += 1
+         #   print 'Game number: ', game_num
+         #   observation = env.reset()
 
 if __name__ == '__main__':
 
@@ -53,9 +85,9 @@ if __name__ == '__main__':
    game        = 'Breakout-v0'  # name of the game ... heh
    game_num    = 1              # always start at first game
    num_actions = 6              # number of actions for the game
-   SIZE        = (100, 100)     # NEW size of the screen input (if RGB don't need 3rd dimension)
+   SIZE        = (84, 84)     # NEW size of the screen input (if RGB don't need 3rd dimension)
    env         = gym.make(game) # create the game
-   play_random = 200            # number of steps to play randomly
+   play_random = 20            # number of steps to play randomly
    seq_length  = 4              # length of sequence for input
    grayscale   = True           # whether or not to use grayscale images instead of rgb
    batch_size  = 1              # size of the batch. Network will receive size batch_size*seq_length*dims
@@ -84,13 +116,12 @@ if __name__ == '__main__':
       if grayscale:
          initial_observation = cv2.cvtColor(initial_observation, cv2.COLOR_RGB2GRAY)
 
-      #for step in tqdm(range(play_random)):
-      for step in range(play_random):
+      for step in tqdm(range(play_random)):
+      #for step in range(play_random):
          env.render() # render environment
          
-         action = env.action_space.sample(seed=ranint(0,100)) # get random action
-         #action = randint(0, num_actions)
-         print action
+         #action = env.action_space.sample(seed=ranint(0,100)) # get random action
+         action = randint(0, num_actions-1)
 
          # execute the random action in the environment, get results
          # current state is the state AFTER taking an action (s_t+1)
@@ -130,17 +161,17 @@ if __name__ == '__main__':
    # faster than python list class
    replay_database = deque(experience_list)
 
+   '''
+   just testing the database
+
    i = 0
    for experience in replay_database:
-      if i < 15:
-         i += 1
-         continue
       print 'previous state: ', experience[0].shape
       print 'action: ', experience[1]
       print 'reward: ', experience[2]
       print 'current_state: ', experience[3].shape
-
       exit()
+   '''
 
    print 'Done with filling database'
    train(replay_database, seq_length, SHAPE, batch_size) 
